aeb3297907e643a935def13ff257c89cb684c8c3
icarodredd <icarog854@gmail.com>
2025-02-24 10:30:01 -0300

    fix: corrige a interface product

--- src/types/index.ts
+++ src/types/index.ts
@@ -25,15 +25,50 @@ export type LayoutRole =
 
 export interface Product {
   id: number;
+  sku: string;
   name: string;
-  description: string;
+  attribute_set_id: number;
   price: number;
-  stock: number;
-  category: string;
-  barcode: string;
-  cost: number;
-  minStock: number;
-  imageUrl: string;
+  status: number;
+  visibility: number;
+  type_id: string;
+  created_at: string;
+  updated_at: string;
+  weight: number;
+  extension_attributes: {
+    website_ids: Array<number>;
+    category_links: Array<{
+      position: number;
+      category_id: string;
+    }>;
+    configurable_product_options: Array<{
+      id: number;
+      attribute_id: string;
+      label: string;
+      position: number;
+      values: Array<{
+        value_index: number;
+      }>;
+      product_id: number;
+    }>;
+    configurable_product_links: Array<number>;
+  };
+  product_links: [];
+  options: [];
+  media_gallery_entries: Array<{
+    id: number;
+    media_type: string;
+    label: string;
+    position: number;
+    disabled: boolean;
+    types: Array<string>;
+    file: string;
+  }>;
+  tier_prices: [];
+  custom_attributes: Array<{
+    attribute_code: string;
+    value: string;
+  }>;
 }
 
 export interface ProductList {
